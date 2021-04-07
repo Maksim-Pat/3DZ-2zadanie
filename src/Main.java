1ece40412e8d01aef3d1d0a2d42b425655460605
Maksim Patrakeev <patrakeev119@gmail.com>
2021-04-07 23:27:10 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        long startScoreClient = 100;
+        long finalScoreClient = 1211;
+        long refill = 1100;
+        int bonus = 100;
+        long finalBonus = refill / bonus;
+        System.out.println(finalBonus);
+    }
+}
+
