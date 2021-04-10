0b168b42af95b031ced12ad1fbcb16f853645878
Maksim Patrakeev <patrakeev119@gmail.com>
2021-04-10 14:36:01 +0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         long startScoreClient = 100;
-        long finalScoreClient = 1211;
         long refill = 1100;
-        int bonus = 100;
-        long finalBonus = refill / bonus;
-        System.out.println(finalBonus);
+        int bonus = 0;
+        if (refill >= 1000) {
+            bonus = (int) (refill / 100);
+        }
+        startScoreClient = startScoreClient + refill + bonus;
+        System.out.println("bonus: " + bonus);
+        System.out.println("totalScore: " + startScoreClient);
     }
 }
 
